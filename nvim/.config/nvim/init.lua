adfb4cc4e73852bf6e22dc9764293028608a7b08
tlj <tlj@tlj.no>
2022-12-29 20:05:20 +0100

    Switched nvim package manager to lazy.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,78 +1,207 @@
--- Install packer
 local vim = vim
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd [[packadd packer.nvim]]
+
+-- Install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
 end
+vim.opt.runtimepath:prepend(lazypath)
 
-require('packer').startup(function(use)
-  -- Packer package manager
-  use 'wbthomason/packer.nvim'
+-- set leader to ' '
+vim.g.mapleader = " "
+vim.opt.termguicolors = true
 
+require("lazy").setup({
   -- catppuccin theme
-  use { "catppuccin/nvim", as = "catppuccin" }
+  "catppuccin/nvim",
+
+  -- neovim dev stuff
+  --"folke/neodev.vim",
+  "milisims/nvim-luaref",
+
+  -- feline status bar
+  {
+    "feline-nvim/feline.nvim",
+    config = function()
+      require('feline').setup()
+      require('feline').winbar.setup()
+    end
+  },
 
   -- DAP debugger
-  use 'mfussenegger/nvim-dap'
-  use 'theHamsta/nvim-dap-virtual-text'
-  use 'leoluz/nvim-dap-go'
-  use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
-  use 'nvim-telescope/telescope-dap.nvim'
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "theHamsta/nvim-dap-virtual-text",
+      {
+        "leoluz/nvim-dap-go",
+        config = function()
+          require('dap-go').setup()
+        end
+      },
+      {
+        "rcarriga/nvim-dap-ui",
+        config = function()
+          require("dapui").setup()
+        end
+      },
+      {
+        "nvim-telescope/telescope-dap.nvim",
+        dependencies = {
+          "nvim-telescope/telescope.nvim",
+        },
+        config = function()
+          require("telescope").load_extension("dap")
+        end
+      },
+    },
+    keys = {
+      { '<leader>db', '<cmd>lua require"dap".toggle_breakpoint()<CR>' },
+      { '<leader>dc', '<cmd>lua require"dap".continue()<CR>' },
+      { '<leader>ds', '<cmd>lua require"dap".stop()<CR>' },
+      { '<leader>do', '<cmd>lua require"dap".step_over()<CR>' },
+      { '<leader>di', '<cmd>lua require"dap".step_into()<CR>' },
+      { '<leader>dt', '<cmd>Telescope dap commands<CR>' },
+      { '<leader>dap', '<cmd>lua require"dapui".toggle()<cr>' },
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
 
-  -- feline status line
-  use 'feline-nvim/feline.nvim'
+      dap.adapters.php = {
+        type = 'executable',
+        command = 'node',
+        args = { '/Users/tjohnsen/src/vscode-php-debug/out/phpDebug.js' }
+      }
+
+      dap.configurations.php = {
+        {
+          type = 'php',
+          request = 'launch',
+          name = 'Listen for Xdebug',
+          port = 8090
+        }
+      }
+
+      vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
+      vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
+    end
+  },
 
   -- git tools
-  use { 'kessejones/git-blame-line.nvim' }
-
-  -- for lua dev
-  use { 'milisims/nvim-luaref' }
-  use { 'folke/neodev.nvim' }
-
-  -- LSP tool to toggle LSP diag features (underlines, etc) on/off
-  -- use 'WhoIsSethDaniel/toggle-lsp-diagnostics.nvim'
-
-  -- Filetree window
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  {
+    "kessejones/git-blame-line.nvim",
+    keys = {
+      { "<leader>gb", "<cmd>GitBlameLineToggle<cr>", desc = "Toggle inline git blame." },
     },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  }
+    config = function()
+      -- Git blamer inline
+      require 'git-blame-line'.setup({
+        git = {
+          default_message = 'Not committed yet',
+          blame_format = '%an - %ar - %s' -- see https://git-scm.com/docs/pretty-formats
+        },
+        view = {
+          left_padding_size = 5,
+          enable_cursor_hold = false
+        }
+      })
+    end
+  },
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = {
-      {
-        'nvim-lua/plenary.nvim',
-        'sharkdp/fd',
-        'nvim-tree/nvim-web-devicons'
-      }
-    }
-  }
+  -- filetree
+  {
+    "nvim-tree/nvim-tree.lua",
+    keys = {
+      { "<leader>tt", '<cmd>NvimTreeToggle<cr>', desc = "Nvim-Tree" },
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require('nvim-tree').setup()
+    end
+  },
 
-  -- Show the undo tree with Telescope
-  use {
-    'debugloop/telescope-undo.nvim',
-    requires = { 'nvim-telescope/telescope.nvim' },
+  -- telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sharkdp/fd",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          undo = {
+            side_by_side = true,
+            layout_strategy = "vertical",
+            layout_config = {
+              preview_height = 0.8,
+            },
+          },
+        },
+      })
+    end,
+    keys = {
+      {'<leader>ff', '<cmd>lua require"telescope.builtin".find_files({hidden = true})<cr>' },
+      {'<leader>fg', '<cmd>lua require"telescope.builtin".live_grep()<cr>' },
+      {'<leader>fs', '<cmd>lua require"telescope.builtin".grep_string()<cr>' },
+      {'<leader>fb', '<cmd>lua require"telescope.builtin".buffers()<cr>' },
+      {'<leader>fh', '<cmd>lua require"telescope.builtin".help_tags()<cr>' },
+      {'<leader>gs', '<cmd>lua require"telescope.builtin".git_status()<cr>' },
+      {'<leader>td', '<cmd>Telescope diagnostics<cr>' },
+      {"<leader>u", "<cmd>Telescope undo<cr>" },
+      {'gr', require('telescope.builtin').lsp_references },
+    },
+  },
+
+  -- Telescope undo
+  {
+    "debugloop/telescope-undo.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
     config = function()
       require("telescope").load_extension("undo")
-      -- optional: vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
-    end,
-  }
+    end
+  },
 
   -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require 'nvim-treesitter.configs'.setup {
+        ensure_installed = "all", -- one of "all", "language", or a list of languages
+        highlight = {
+          enable = true, -- false will disable the whole extension
+          disable = {}, -- list of language that will be disabled
+        },
+      }
+    end
+  },
 
   -- LSP Zero config to automatically install language server for languages
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    requires = {
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    dependencies = {
       -- LSP Support
       { 'neovim/nvim-lspconfig' },
       { 'williamboman/mason.nvim' },
@@ -89,126 +218,71 @@ require('packer').startup(function(use)
       -- Snippets
       { 'L3MON4D3/LuaSnip' },
       { 'rafamadriz/friendly-snippets' },
-    }
-  }
-
-  -- Show a floating preview for a definition
-  use {
-    'rmagatti/goto-preview',
+    },
     config = function()
-      require('goto-preview').setup {}
+      -- LSP init
+      local lsp = require('lsp-zero')
+      lsp.preset('recommended')
+      lsp.setup()
+
+      require 'lspconfig'.sumneko_lua.setup {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' }
+            }
+          }
+        }
+      }
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+        end,
+      })
     end
-  }
-
-  use {
-    'tlj/sapi-preview.nvim',
-    requires = {
-      'kkharji/sqlite.lua'
-    }
-  }
-
-  if is_bootstrap then
-    require('packer').sync()
-  end
-end)
-
-if is_bootstrap then
-  print '=================================='
-  print '    Plugins are being installed'
-  print '    Wait until Packer completes,'
-  print '       then restart nvim'
-  print '=================================='
-  return
-end
-
--- LSP init
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
-lsp.setup()
-
--- Disabled this and plugin. Not using it right now.
--- require 'toggle_lsp_diagnostics'.init()
-
--- DAP Debugger
-require('dap-go').setup()
-require('dapui').setup()
-require('telescope').load_extension('dap')
-
-local dap, dapui = require("dap"), require("dapui")
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
-
-dap.adapters.php = {
-  type = 'executable',
-  command = 'node',
-  args = { '/Users/tjohnsen/src/vscode-php-debug/out/phpDebug.js' }
-}
-
-dap.configurations.php = {
-  {
-    type = 'php',
-    request = 'launch',
-    name = 'Listen for Xdebug',
-    port = 8090
-  }
-}
-
-vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
-vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
-
--- Git blamer inline
-require 'git-blame-line'.setup({
-  git = {
-    default_message = 'Not committed yet',
-    blame_format = '%an - %ar - %s' -- see https://git-scm.com/docs/pretty-formats
   },
-  view = {
-    left_padding_size = 5,
-    enable_cursor_hold = false
+
+  -- show a floating preview for definition
+  {
+    "rmagatti/goto-preview",
+    keys = {
+      { "gp", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>" },
+    },
+    config = function()
+      require("goto-preview").setup({})
+    end
+  },
+
+  -- Sports API 
+  {
+    "tlj/sapi-preview.nvim",
+    dependencies = {
+      "kkharji/sqlite.lua",
+    },
+    config = function()
+      require("sapi-preview").setup()
+    end,
+    keys = {
+      { "<leader>sg", "<cmd>SapiGoto<cr>", desc = "Open SAPI endpoints valid for URN on cursor." },
+      { "<leader>sr", "<cmd>SapiRecents<cr>", desc = "Open list of recently opened SAPI endpoints." },
+      { "<leader>se", "<cmd>SapiEndpoints<cr>", desc = "Open list of endpoints for current package." },
+      { "<leader>su", "<cmd>SapiRefresh<cr>", desc = "Refresh list of endpoints for current package from selected base URL." },
+      { "<leader>sp", "<cmd>SapiPackage<cr>", desc = "Select a SAPI package." },
+      { "<leader>sb", "<cmd>SapiBaseUrl<cr>", desc = "Select a SAPI base URL." },
+    },
   }
 })
+
 
 -- Theme
 vim.opt.signcolumn = 'yes'
-vim.opt.termguicolors = true
-
 vim.cmd('colorscheme catppuccin-mocha')
-
--- Feline bar
-require('feline').setup()
-require('feline').winbar.setup()
-
--- undo
-require("telescope").setup({
-  extensions = {
-    undo = {
-      side_by_side = true,
-      layout_strategy = "vertical",
-      layout_config = {
-        preview_height = 0.8,
-      },
-    },
-  },
-})
-
--- Other plugins
-require('nvim-tree').setup()
-
--- TreeSitter
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = "all", -- one of "all", "language", or a list of languages
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = {}, -- list of language that will be disabled
-  },
-}
 
 -- Folding through TreeSitter
 local opt = vim.opt
@@ -236,59 +310,4 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
---
-require 'lspconfig'.sumneko_lua.setup {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      }
-    }
-  }
-}
 
--- sapi preview
-require('sapi-preview').setup()
-vim.keymap.set('n', '<leader>sg', '<cmd>SapiGoto<cr>', {})
-vim.keymap.set('n', '<leader>sr', '<cmd>SapiRecents<cr>', {})
-vim.keymap.set('n', '<leader>se', '<cmd>SapiEndpoints<cr>', {})
-vim.keymap.set('n', '<leader>su', '<cmd>SapiRefresh<cr>', {})
-vim.keymap.set('n', '<leader>sp', '<cmd>SapiPackage<cr>', {})
-vim.keymap.set('n', '<leader>sb', '<cmd>SapiBaseUrl<cr>', {})
-
--- Maps
-local map = vim.api.nvim_set_keymap
-map('n', '<leader>db', '<cmd>lua require"dap".toggle_breakpoint()<CR>', { noremap = true, silent = false })
-map('n', '<leader>dc', '<cmd>lua require"dap".continue()<CR>', { noremap = true, silent = false })
-map('n', '<leader>ds', '<cmd>lua require"dap".stop()<CR>', {})
-map('n', '<leader>do', '<cmd>lua require"dap".step_over()<CR>', { noremap = true, silent = false })
-map('n', '<leader>di', '<cmd>lua require"dap".step_into()<CR>', { noremap = true, silent = false })
-map('n', '<leader>dt', '<cmd>Telescope dap commands<CR>', {})
-map('n', '<leader>dap', '<cmd>lua require"dapui".toggle()<cr>', {})
-
-map('n', '<leader>ff', '<cmd>lua require"telescope.builtin".find_files({hidden = true})<cr>', {})
-map('n', '<leader>fg', '<cmd>lua require"telescope.builtin".live_grep()<cr>', {})
-map('n', '<leader>fs', '<cmd>lua require"telescope.builtin".grep_string()<cr>', {})
-map('n', '<leader>fb', '<cmd>lua require"telescope.builtin".buffers()<cr>', {})
-map('n', '<leader>fh', '<cmd>lua require"telescope.builtin".help_tags()<cr>', {})
-map('n', '<leader>gs', '<cmd>lua require"telescope.builtin".git_status()<cr>', {})
-map('n', '<leader>gb', '<cmd>GitBlameLineToggle<cr>', {})
-
-map('n', '<leader>tt', '<cmd>NvimTreeToggle<cr>', {})
-map('n', '<leader>td', '<cmd>Telescope diagnostics<cr>', {})
-map("n", "<leader>u", "<cmd>Telescope undo<cr>", {})
-
--- lsp
-vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-
-vim.keymap.set('n', 'gp', "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", {})
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
-  end,
-})
