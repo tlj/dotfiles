cd95dd9b00a3e4f085a1c7c8217a2bc3bb682687
tlj <tlj@tlj.no>
2023-01-05 10:24:34 +0100

    nvim; use '<leader>sa' for select_api in endpoint previwer

--- nvim/.config/nvim/lua/user/plugins/endpoint-previewer.lua
+++ nvim/.config/nvim/lua/user/plugins/endpoint-previewer.lua
@@ -11,7 +11,7 @@ local M = {
     { "<leader>sr", "<cmd>EndpointRecents<cr>", desc = "Open list of recently opened SAPI endpoints." },
     { "<leader>se", "<cmd>EndpointEndpoints<cr>", desc = "Open list of endpoints for current package." },
     { "<leader>su", "<cmd>EndpointRefresh<cr>", desc = "Refresh list of endpoints for current package from selected base URL." },
-    { "<leader>sp", "<cmd>EndpointAPI<cr>", desc = "Select an API." },
+    { "<leader>sa", "<cmd>EndpointAPI<cr>", desc = "Select an API." },
     { "<leader>sb", "<cmd>EndpointBaseUrl<cr>", desc = "Select a SAPI base URL." },
   },
 }
