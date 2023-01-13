8b9519cb511f96083602d0cad6e37aa36dccfabf
tlj <tlj@tlj.no>
2023-01-13 13:42:13 +0100

    fix(nvim): Updated mappings for endpoint-previewer

--- nvim/.config/nvim/lua/user/plugins/endpoint-previewer.lua
+++ nvim/.config/nvim/lua/user/plugins/endpoint-previewer.lua
@@ -7,12 +7,13 @@ local M = {
     require("endpoint-previewer").setup()
   end,
   keys = {
-    { "<leader>sg", "<cmd>EndpointGoto<cr>", desc = "Open SAPI endpoints valid for URN on cursor." },
-    { "<leader>sr", "<cmd>EndpointRecents<cr>", desc = "Open list of recently opened SAPI endpoints." },
-    { "<leader>se", "<cmd>EndpointEndpoints<cr>", desc = "Open list of endpoints for current package." },
-    { "<leader>su", "<cmd>EndpointRefresh<cr>", desc = "Refresh list of endpoints for current package from selected base URL." },
+    { "<leader>sg", "<cmd>EndpointGoto<cr>", desc = "Open API endpoints valid for replacement text on cursor." },
+    { "<leader>sr", "<cmd>EndpointRecents<cr>", desc = "Open list of recently opened API endpoints." },
+    { "<leader>se", "<cmd>EndpointEndpoints<cr>", desc = "Open list of endpoints for current API." },
+    { "<leader>su", "<cmd>EndpointRefresh<cr>", desc = "Refresh list of APIs and Endpoints." },
     { "<leader>sa", "<cmd>EndpointAPI<cr>", desc = "Select an API." },
-    { "<leader>sb", "<cmd>EndpointBaseUrl<cr>", desc = "Select a SAPI base URL." },
+    { "<leader>sd", "<cmd>EndpointSelectEnv<cr>", desc = "Select environment." },
+    { "<leader>sx", "<cmd>EndpointSelectRemoteEnv<cr>", desc = "Select remote environment." },
   },
 }
 
