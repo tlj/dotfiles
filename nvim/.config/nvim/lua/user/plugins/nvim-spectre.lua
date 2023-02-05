f32171ab92512d1a880d30814bb317127a0a37b7
tlj <tlj@tlj.no>
2023-02-05 14:35:56 +0100

    fix(nvim): fix for 'fc' description

--- nvim/.config/nvim/lua/user/plugins/nvim-spectre.lua
+++ nvim/.config/nvim/lua/user/plugins/nvim-spectre.lua
@@ -3,7 +3,7 @@ local M = {
   keys = {
     { "<leader>fr", function() require("spectre").open() end, desc = "Find/Replace in files (Spectre)" },
     { "<leader>fw", '<cmd>lua require("spectre").open_visual({select_word=true})<cr>', desc = "Find/Replace current word in files." },
-    { "<leader>fc", 'viw:lua require("spectre").open_file_search()<cr>', 'Search in current file.' },
+    { "<leader>fc", 'viw:lua require("spectre").open_file_search()<cr>', desc = 'Search in current file.' },
   },
   enabled = true,
 }
