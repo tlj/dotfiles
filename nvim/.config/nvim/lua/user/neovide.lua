8da0e009ae9b2d2e5192396f09431d78e69d3b42
tlj <tlj@tlj.no>
2023-10-10 22:52:33 +0200

    feat(neovide): added vfx mode pixiedust

--- nvim/.config/nvim/lua/user/neovide.lua
+++ nvim/.config/nvim/lua/user/neovide.lua
@@ -10,6 +10,7 @@ vim.g.neovide_cursor_animation_length = 0.05
 vim.g.neovide_cursor_trail_size = 0.2
 vim.g.neovide_cursor_antialiasing = true
 vim.g.neovide_cursor_animate_in_insert_mode = false
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
 
 vim.g.neovide_confirm_quit = true
 vim.g.neovide_fullscreen = false
