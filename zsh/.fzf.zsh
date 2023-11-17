04a516ce1f7be81f05ec37f678df59f26d61d995
tlj <tlj@tlj.no>
2023-11-17 10:07:35 +0100

    feat(tmux): use ctrl-e as tmux prefix as to not collide with fzf

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -4,6 +4,8 @@ if [[ ! "$PATH" == */Users/tjohnsen/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
+FZF_DEFAULT_OPTS="--preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
