b7016b09b5cda6cc4e48db28e0a235849d30ea72
tlj <tlj@tlj.no>
2023-11-21 20:51:49 +0100

    fix(nvim): renamed api-browser plugin

---
+++ nvim/.config/nvim/lua/plugins/api-browser.lua
@@ -0,0 +1,25 @@
+local M = {
+  "tlj/api-browser.nvim",
+  dependencies = {
+    "kkharji/sqlite.lua",
+  },
+  dev = true,
+  enabled = function ()
+    return true
+    -- return os.getenv("NVIM_API_BROWSER_URLS") ~= ""
+  end,
+  config = function()
+    require("api-browser").setup()
+  end,
+  keys = {
+    { "<leader>sg", "<cmd>ApiBrowserGoto<cr>", desc = "Open API endpoints valid for replacement text on cursor." },
+    { "<leader>sr", "<cmd>ApiBrowserRecents<cr>", desc = "Open list of recently opened API endpoints." },
+    { "<leader>se", "<cmd>ApiBrowserEndpoints<cr>", desc = "Open list of endpoints for current API." },
+    { "<leader>su", "<cmd>ApiBrowserRefresh<cr>", desc = "Refresh list of APIs and Endpoints." },
+    { "<leader>sa", "<cmd>ApiBrowserAPI<cr>", desc = "Select an API." },
+    { "<leader>sd", "<cmd>ApiBrowserSelectEnv<cr>", desc = "Select environment." },
+    { "<leader>sx", "<cmd>ApiBrowserSelectRemoteEnv<cr>", desc = "Select remote environment." },
+  },
+}
+
+return M
