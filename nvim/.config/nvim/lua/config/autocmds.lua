f21b2b0e6348a78821737b0e558b65ba07fdaba4
tlj <tlj@tlj.no>
2023-11-29 07:53:01 +0100

    feat(nvim): added x-ray/go, neotest-go and autocmd for go
    Not sure if x-ray/go is really needed, but hey, there you go

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,15 @@
 -- check if we need to reload the file when it changes
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, { command = "checktime"} )
 
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
