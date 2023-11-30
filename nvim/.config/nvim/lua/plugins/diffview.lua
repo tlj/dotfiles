cda94654a8b2e7abbc106a61d597d82fe48c3a7b
tlj <tlj@tlj.no>
2023-11-30 08:04:54 +0100

    feat(nvim): added git diffview plugin
    # Conflicts:
    #	nvim/.config/nvim/lazy-lock.json

---
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,10 @@
+return {
+  "sindrets/diffview.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>hh", "<cmd>DiffviewOpen<cr>" },
+    { "<leader>hf", "<cmd>DiffviewFileHistory<cr>" },
+    { "<leader>hc", "<cmd>DiffviewClose<cr>" },
+  },
+  opts = {},
+}
