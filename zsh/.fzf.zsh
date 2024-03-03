009f9e1f7a440154582728ee673d90d0d588c233
tlj <tlj@tlj.no>
2024-03-03 07:40:41 +0100

    fix(fzf): removed preview from defaults in fzf as it is not recommended

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -4,7 +4,9 @@ if [[ ! "$PATH" == */.fzf/bin* ]]; then
   PATH="${HOME}/.fzf/bin:${PATH:+${PATH}:}"
 fi
 
-FZF_DEFAULT_OPTS="--preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+#FZF_DEFAULT_OPTS="--preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+FZF_DEFAULT_OPTS="--reverse --border rounded --no-info --pointer='>' --marker='+' --ansi --color='16,bg+:-1,gutter:-1,prompt:5,pointer:5,marker:6,border:4,label:4'"
+FZF_CTRL_R_OPTS="--preview 'echo {}' --border-label=' history ' --prompt '🕑 '"
 
 # Auto-completion
 # ---------------
