735b665d2d1c633d79e9dab2346f421d5fdb55d6
tlj <tlj@tlj.no>
2024-03-29 07:21:13 +0100

    fix(nvim): use alt-j to add all to quicklist in fzf, alt-k quickfix

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -3,6 +3,7 @@ return {
 	enabled = require("config.util").is_enabled("ibhagwan/fzf-lua"),
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local actions = require("fzf-lua.actions")
 		require("fzf-lua").setup({
 			"default",
 			winopts = {
@@ -28,6 +29,43 @@ return {
 			},
 			grep = {
 				rg_opts = "--column --hidden --line-number --no-heading --color=always --smart-case --ignore-file ~/.config/nvim/scripts/rgignore --max-columns=4096 -e",
+			},
+			actions = {
+				files = {
+					-- providers that inherit these actions:
+					--   files, git_files, git_status, grep, lsp
+					--   oldfiles, quickfix, loclist, tags, btags
+					--   args
+					-- default action opens a single selection
+					-- or sends multiple selection to quickfix
+					-- replace the default action with the below
+					-- to open all files whether single or multiple
+					-- ["default"]     = actions.file_edit,
+					["default"] = actions.file_edit_or_qf,
+					["ctrl-s"] = actions.file_split,
+					["ctrl-v"] = actions.file_vsplit,
+					["ctrl-t"] = actions.file_tabedit,
+					["alt-k"] = actions.file_sel_to_qf,
+					["alt-q"] = actions.file_sel_to_qf,
+					["alt-l"] = actions.file_sel_to_ll,
+				},
+			},
+			keymap = {
+				fzf = {
+					-- fzf '--bind=' options
+					["ctrl-z"] = "abort",
+					["ctrl-u"] = "unix-line-discard",
+					["ctrl-f"] = "half-page-down",
+					["ctrl-b"] = "half-page-up",
+					["ctrl-a"] = "beginning-of-line",
+					["ctrl-e"] = "end-of-line",
+					["alt-j"] = "toggle-all",
+					-- Only valid with fzf previewers (bat/cat/git/etc)
+					["f3"] = "toggle-preview-wrap",
+					["f4"] = "toggle-preview",
+					["shift-down"] = "preview-page-down",
+					["shift-up"] = "preview-page-up",
+				},
 			},
 		})
 	end,
