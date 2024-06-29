faf32c7109ba68722457a762be63a503475116ef
tlj <tlj@tlj.no>
2024-06-29 22:18:44 +0200

    fix(install): tweaks for install script

--- install.sh
+++ install.sh
@@ -2,13 +2,16 @@
 
 set -e 
 
-OS=$(uname)
-if [[ "$OS" == "Linux" ]]; then
-  sudo apt install -y git
+git --version >/dev/null
+if [[ $? -ne 0 ]]; then
+  echo "Git is not installed. Please install git and try again."
+  exit 1
 fi
 
 echo "Cloning dotfiles..." 
 git clone -b master https://github.com/tlj/dotfiles.git ~/dotfiles > /dev/null
 
-cd dotfiles
+cd ~/dotfiles
 source ./setup.sh
+
+
