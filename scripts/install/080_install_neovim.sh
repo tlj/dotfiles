da162dbb15b456f6393f454bcee4bb6f82f531ef
tlj <tlj@tlj.no>
2024-06-30 08:32:17 +0200

    fix: npm in neovim

--- scripts/install/080_install_neovim.sh
+++ scripts/install/080_install_neovim.sh
@@ -21,7 +21,7 @@ else
   sudo apt-get install -y ripgrep fd-find fzf luarocks
   sudo luarocks install luacheck
 
-  eval "$(mise activate bash)"
+  eval "$(~/.local/bin/mise activate bash)"
   npm install -g tree-sitter-cli
 
   echo "Installing viu..."
