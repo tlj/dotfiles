2093e83a741f0100bc52d643be04daeb7b8c747b
tlj <tlj@tlj.no>
2024-08-11 18:45:10 +0200

    fix(install): fzf tar gz, not zip

--- scripts/install/080_install_fzf.sh
+++ scripts/install/080_install_fzf.sh
@@ -5,7 +5,7 @@
 . scripts/lib/install_github_release.sh
 
 echo "Installing fzf binary..."
-install_github_release junegunn/fzf fzf-:VERSION_NUM:-${PLATFORM}_${ARCH}.zip
+install_github_release junegunn/fzf fzf-:VERSION_NUM:-${PLATFORM}_${ARCH}.tar.gz
 
 echo "Installing fzf from git to get bin/ scripts..."
 install_with_git ~/.fzf https://github.com/junegunn/fzf.git
