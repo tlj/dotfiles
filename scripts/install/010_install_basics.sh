65eaca26b935e815210c7d8f01a899c5469f72b6
tlj <tlj@tlj.no>
2024-08-11 17:50:43 +0200

    fix(install): btop arch for amd64

--- scripts/install/010_install_basics.sh
+++ scripts/install/010_install_basics.sh
@@ -12,7 +12,11 @@ else
   sudo apt install -y curl stow fuse libfuse2t64 bat sqlite3 cmake
 
   echo "Installing btop..."
-  install_github_release aristocratos/btop btop-${ARCH}-linux-musl.tbz
+  BTOPARCH=$ARCH
+  if [[ $ARCH == "amd64" ]]; then
+    BTOPARCH="x86_64"
+  fi
+  install_github_release aristocratos/btop btop-${BTOPARCH}-linux-musl.tbz
 
   ln -sfn /usr/bin/batcat ~/.local/bin/bat
 
