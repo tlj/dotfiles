004bf2f4a2b4de195367ae6c3853ff8b0b01296c
tlj <tlj@tlj.no>
2024-09-16 14:44:48 +0200

    feat(nvim): use oil instead of mini.files

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -51,6 +51,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		"tsplayground",
 		"PlenaryTestPopup",
 		"grug-far",
+		"oil",
 	},
 	callback = function(event)
 		vim.bo[event.buf].buflisted = false
--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -18,7 +18,7 @@ return {
 			["g?"] = "actions.show_help",
 			["<CR>"] = "actions.select",
 			["<C-s>"] = "actions.select_vsplit",
-			["<C-h>"] = "actions.select_split",
+			["<C-v>"] = "actions.select_split",
 			["<C-t>"] = "actions.select_tab",
 			["<C-p>"] = "actions.preview",
 			["<C-c>"] = "actions.close",
