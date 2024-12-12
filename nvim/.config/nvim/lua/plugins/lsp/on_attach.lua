f7805726b0b52b5d42551528fda57a3f725ca4c7
tlj <tlj@tlj.no>
2024-12-12 08:45:43 +0100

    fix(nvim): fixed a problem with virtual text in lsp

--- nvim/.config/nvim/lua/plugins/lsp/on_attach.lua
+++ nvim/.config/nvim/lua/plugins/lsp/on_attach.lua
@@ -1,23 +1,23 @@
 local function on_attach(_, bufnr)
-	local bufmap = function(mode, lhs, rhs, desc)
-		local opts = { buffer = bufnr, desc = desc }
-		vim.keymap.set(mode, lhs, rhs, opts)
-	end
-
-	bufmap("n", "gd", "<cmd>FzfLua lsp_definitions<cr>", "Fzf Jump to definition")
-	bufmap("n", "gD", "<cmd>FzfLua lsp_declarations<cr>", "Fzf Jump to declaration")
-	bufmap("n", "gi", "<cmd>FzfLua lsp_implementations<cr>", "Fzf Implementations")
-	bufmap("n", "gr", "<cmd>FzfLua lsp_references<cr>", "Fzf References")
-	bufmap("n", "gl", '<cmd>lua vim.diagnostic.open_float(0, { scope = "line" })<cr>', "Show diagnostics")
-	bufmap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename")
+	vim.keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<cr>", { buffer = bufnr, desc = "Fzf Jump to definition" })
+	vim.keymap.set("n", "gD", "<cmd>FzfLua lsp_declarations<cr>", { buffer = bufnr, desc = "Fzf Jump to declaration" })
+	vim.keymap.set("n", "gi", "<cmd>FzfLua lsp_implementations<cr>", { buffer = bufnr, desc = "Fzf Implementations" })
+	vim.keymap.set("n", "gr", "<cmd>FzfLua lsp_references<cr>", { buffer = bufnr, desc = "Fzf References" })
+	vim.keymap.set(
+		"n",
+		"gl",
+		'<cmd>lua vim.diagnostic.open_float(0, { scope = "line" })<cr>',
+		{ buffer = bufnr, desc = "Show diagnostics" }
+	)
+	vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", { buffer = bufnr, desc = "Rename" })
 
 	local signs = require("config.icons").lsp.diagnostic.signs
-	vim.diagnostic.config({
-		virtual_text = false,
+	local diagnostic_config = {
+		virtual_text = true,
 		underline = true,
 		update_in_insert = false,
 		float = {
-			border = 'single',
+			border = "single",
 		},
 		signs = {
 			text = {
@@ -40,7 +40,11 @@ local function on_attach(_, bufnr)
 			},
 			linehl = {}, -- No line highlighting
 		},
-	})
+	}
+
+	vim.diagnostic.config(diagnostic_config)
+	vim.lsp.handlers["textDocument/publishDiagnostics"] =
+		vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, diagnostic_config)
 end
 
 return {
