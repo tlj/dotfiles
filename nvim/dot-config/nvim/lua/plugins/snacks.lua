effa381c02e081fb74402d092a1197086b7d3f8f
tlj <tlj@tlj.no>
2024-12-17 10:19:55 +0100

    feat(nvim): added lazygit from snacks plugin

---
+++ nvim/dot-config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,39 @@
+plugin("snacks", {
+	setup = function()
+		require("snacks").setup({
+			lazygit = { enabled = true },
+		})
+	end,
+	keys = {
+		["<leader>lg"] = {
+			cmd = function()
+				require("snacks").lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		["<leader>ll"] = {
+			cmd = function()
+				Snacks.lazygit.log()
+			end,
+			desc = "Lazygit Log",
+		},
+		["<leader>lf"] = {
+			cmd = function()
+				Snacks.lazygit.log_file()
+			end,
+			desc = "Lazygit File Log",
+		},
+		["<leader>lb"] = {
+			cmd = function()
+				Snacks.git.blame_line()
+			end,
+			desc = "Lazygit Blame",
+		},
+		["<leader>ft"] = {
+			cmd = function()
+				Snacks.terminal()
+			end,
+			desc = "Terminal",
+		},
+	},
+})
