662187630cb231fd067b668a794d4a3fe13282cb
tlj <tlj@tlj.no>
2024-12-20 09:39:33 +0100

    feat(nvim): updated plugins, removed a reference to blink

--- nvim/dot-config/nvim/update.sh
+++ nvim/dot-config/nvim/update.sh
@@ -3,10 +3,6 @@
 git submodule init
 git submodule update --remote
 
-cd pack/vendor/opt/blink.cmp
-git checkout v0.7.6
-cd - > /dev/null
-
 cd pack/vendor/opt/CopilotChat 
 make tiktoken
 cd - > /dev/null
