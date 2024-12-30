12c113a81486a2b004fa51dc89b52843e4638bf0
tlj <tlj@tlj.no>
2024-12-30 22:49:06 +0100

    fix: use brew to install ghostty, removed neovim plugins from setup.sh as we are using graft-git now

--- scripts/install/106_install_ghostty.sh
+++ scripts/install/106_install_ghostty.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+. scripts/lib/detect_os.sh
 . scripts/lib/print_utils.sh
 
 print_header "Ghostty"
+
+if isMac; then
+  brew install -q --cask ghostty
+fi
 
 stow --target=$HOME --dotfiles -v --restow ghostty/ 
 
