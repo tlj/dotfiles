b6a44cdf0d01435b3db7ced2e40641986b71648e
tlj <tlj@tlj.no>
2025-09-22 14:20:44 +0200

    feat: added wofi and waybar to dotfiles

--- scripts/install/107_install_hyprland.sh
+++ scripts/install/107_install_hyprland.sh
@@ -6,7 +6,7 @@
 print_header "Hyprland"
 
 if isArch; then
-  sudo pacman -S --noconfirm --quiet hyprland
+  sudo pacman -S --noconfirm --quiet hyprland waybar wofi
   stow --target=$HOME --dotfiles -v --restow hypr/ waybar/ wofi/
 fi
 
